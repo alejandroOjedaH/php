029a593831b9ae1f1f1633ab2999d057f0b74167
alejandro <limonmagico1@gmail.com>
2023-12-19 18:37:42 +0100

    Estoouj

--- practica16/js/principal.js
+++ practica16/js/principal.js
@@ -1,7 +1,39 @@
 window.onload= ()=>{
-    login();
+const cuerpo = document.getElementsByTagName("body")[0];
+const logeoPag = document.getElementById("login");
+
+
+cargarPantallaLogin();
+
+function cargarPantallaLogin(){
+    ocultar();
+    cuerpo.innerHTML=logeoPag.innerHTML;
+    const logeoBut = document.getElementById("logear");
+
+    logeoBut.onclick = function(){
+        const usuario = document.getElementById("usuario");
+        const clave = document.getElementById("contrasenna");
+        let datos ={
+            usuario: usuario.value,
+            password: clave.value
+        }
+
+        fetch("http://localhost/php/practica15/api",{method:"POST", headers:{"Content-Type":"application/json"}, body: JSON.stringify(datos)})
+        .then(response => {
+            if(!response.ok){
+                console.error("Error Logeo");
+            }else{
+                return response.json;
+            }
+        })
+        .then(esto =>{
+            console.log(esto.toString());
+        })
+    }
 }
 
-function login() {
-    
+function ocultar(){
+    cuerpo.innerHTML="";
 }
+}
+
