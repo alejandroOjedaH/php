6da6903265cea95128d5fd894097c1b51bbcb5bf
alejandro <limonmagico1@gmail.com>
2023-12-20 21:43:54 +0100

    wqeasd

--- practica16/js/principal.js
+++ practica16/js/principal.js
@@ -1,4 +1,5 @@
 window.onload= ()=>{
+    let token = null;
     const cuerpo = document.getElementsByTagName("body")[0];
     const logeoPag = document.getElementById("login");
     const principalPag = document.getElementById("principal");
@@ -19,7 +20,7 @@ window.onload= ()=>{
                 password: clave.value
             }
 
-            fetch("http://localhost/php/practica15/api",{method:"POST", headers:{"Content-Type":"application/json"}, body: JSON.stringify(datos)})
+            fetch("http://localhost/php/practica14/api",{method:"POST", headers:{"Content-Type":"application/json"}, body: JSON.stringify(datos)})
             .then(response => {
                 if(!response.ok){
                     console.error("Error Logeo");
@@ -39,28 +40,49 @@ window.onload= ()=>{
         ocultar();
         mostrarCabecera();
         cuerpo.innerHTML+=principalPag.innerHTML;
-        
-    }
+        const lista = document.getElementById("listaCategorias");
 
-    function ocultar(){
-        cuerpo.innerHTML="";
+        fetch("http://localhost/php/practica14/api/categorias",{method:"GET"})
+            .then(response => {
+                if(!response.ok){
+                    console.error("Error Cargar Categorias");
+                }else{
+                    return response.json();
+                }
+            })
+            .then(esto =>{
+                esto.forEach(cosa => {
+                    const elemento = document.createElement("li");
+                    elemento.innerText = cosa.descripcion;
+                    elemento.onclick = ()=>{ mostrarProducto(cosa.codCat)};
+                    lista.appendChild(elemento);
+                });
+            });
     }
 
     function mostrarCabecera(){
         let cabecera = document.createElement("div");
-        let categoria =document.createElement("span");
+        let categoria =document.createElement("a");
         let carrito =document.createElement("a");
         let sesion =document.createElement("a");
 
         cabecera.id= "cabecera";
         
-        categoria.innerText="Categorias";
-        carrito.innerText="Categorias";
-        sesion.innerText="Categorias";
+        categoria.innerText="Categorias ";
+        carrito.innerText="Ver carrito ";
+        sesion.innerText="Cerrar Sesion";
+
+        categoria.onclick = mostrarCategoria;
+        carrito.onclick = mostrarCarrito;
+        sesion.onclick = mostrarSesion;
 
         cabecera.appendChild(categoria);
         cabecera.appendChild(carrito);
         cabecera.appendChild(sesion);
-        cuerpo.innerHTML=cabecera;
+        cuerpo.innerHTML=cabecera.innerHTML;
+    }
+
+    function ocultar(){
+        cuerpo.innerHTML="";
     }
 }
